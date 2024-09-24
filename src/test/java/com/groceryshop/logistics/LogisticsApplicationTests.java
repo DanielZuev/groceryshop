175e9c094bbd79e76370e00726c816652136d8b4
danielzuev <up2016078@myport.ac.uk>
2024-09-24 13:48:06 +0100

    initial commit

---
+++ src/test/java/com/groceryshop/logistics/LogisticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.groceryshop.logistics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
